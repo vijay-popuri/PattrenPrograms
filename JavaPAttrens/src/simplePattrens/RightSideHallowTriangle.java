f938dc6fc43ce7deee43bf7467904d907e36189d
vijay <vijaypopuri2021@gmail.com>
2025-01-02 08:36:48 +0530

    halloChngs

---
+++ JavaPAttrens/src/simplePattrens/RightSideHallowTriangle.java
@@ -0,0 +1,18 @@
+package simplePattrens;
+
+public class RightSideHallowTriangle {
+
+	public static void main(String[] args) {
+		int n = 10;
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= n; j++) {
+				if (i == (n - (n - 1)) || (i == j) || j == n) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
