b25b2fa0bd4039755694aeec6e2c2abb3d44889e
vijay <vijaypopuri2021@gmail.com>
2025-01-14 15:41:07 +0530

    CorrectCode

--- JavaPAttrens/src/LeetcodeProgs/ZigZagPattren.java
+++ JavaPAttrens/src/LeetcodeProgs/ZigZagPattren.java
@@ -9,25 +9,39 @@ public class ZigZagPattren {
 		List<Integer> list = new ArrayList<>();
 		boolean leftToRight = true;
 		for (int i = 0; i < a.length; i++) {
-			if (leftToRight) {
-				for (int j = 0; j < a[i].length; j += 2) {
-					list.add(a[i][j]);
+			if (i % 2 == 0) {
+				for (int j = 0; j < a[i].length; j++) {
+					if (leftToRight) {
+
+						list.add(a[i][j]);
+
+					}
+					leftToRight = !leftToRight;
+
 				}
 			} else {
-				for (int j = a[i].length - 1; j >= 0; j -= 2) {
-					list.add(a[i][j]);
+				for (int j = a[i].length - 1; j >= 0; j--) {
+					if (leftToRight) {
+						list.add(a[i][j]);
+					}
+					leftToRight = !leftToRight;
+
 				}
 
 			}
-			leftToRight = !leftToRight;
+
 		}
 
 		return list;
 	}
-//output{1,3,8,6,9,11,16,14}
 
+//output{1,3,8,6,9,11,16,14}
+//1 3 6
 	public static void main(String[] args) {
-		int[][] grid = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+		int[][] grid = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}};
 		ZigZagPattren obj = new ZigZagPattren();
 
 		List<Integer> zigZagEles = obj.getTheZigZagEles(grid);
